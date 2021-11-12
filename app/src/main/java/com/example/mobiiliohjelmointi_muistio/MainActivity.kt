5c19a2ed68d5a59aa3b8b6a9b662f9770fc56f66
Tomi Väisänen <tomi.vaisanen@hurja.fi>
2021-11-12 09:16:37 +0200

    add: listview query

--- app/src/main/java/com/example/mobiiliohjelmointi_muistio/MainActivity.kt
+++ app/src/main/java/com/example/mobiiliohjelmointi_muistio/MainActivity.kt
@@ -1,27 +1,89 @@
 package com.example.mobiiliohjelmointi_muistio
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val artNameArray = ArrayList<String>()
+        val artImageArray = ArrayList<Bitmap>()
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, artNameArray)
+
+        listView.adapter = arrayAdapter
+
+        try {
+            //Luodaan database
+            val database = this.openOrCreateDatabase("Notes", Context.MODE_PRIVATE, null)
+            //Luodaan taulut
+            database.execSQL("create table if not exists note(title varchar, image blob, content varchar)")
+
+            //SQL query
+            val cursor = database.rawQuery("select * from note", null)
+
+            //Otetaan nimi muuttujaan
+            val nameidx = cursor.getColumnIndex("title")
+
+            //Otetaan kuva muuttujaan
+            val imageidx = cursor.getColumnIndex("image")
+
+            //Siirretään cursori ensimmäiseen elementtiin
+            cursor.moveToFirst()
+
+            //Loopataan elementit läpi
+            while (cursor != null) {
+                artNameArray.add(cursor.getString(nameidx))
+                val byteArray = cursor.getBlob(imageidx)
+                val image = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+
+                artImageArray.add(image)
+
+                cursor.moveToNext()
+
+                arrayAdapter.notifyDataSetChanged()
+
+            }
+
+            cursor?.close()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
+            val intent = Intent(applicationContext, SecondScreen::class.java)
+            intent.putExtra("name", artNameArray[i])
+            intent.putExtra("info", "old")
+
+            val chosen = Globals.Chosen
+            chosen.chosenImage = artImageArray[i]
+
+            startActivity(intent)
+        }
     }
 
+    //Luodaan menu
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-
         val menuInflater = menuInflater
         menuInflater.inflate(R.menu.add_note, menu)
-
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
+        //Lisää muistiinpanoa klikkaamalla tapahtuva asia
         if (item.itemId == R.id.add_note) {
             val intent = Intent(applicationContext, SecondScreen::class.java)
             intent.putExtra("info", "new")
--- app/src/main/java/com/example/mobiiliohjelmointi_muistio/SecondScreen.kt
+++ app/src/main/java/com/example/mobiiliohjelmointi_muistio/SecondScreen.kt
@@ -20,6 +20,7 @@ import java.lang.Exception
 
 class SecondScreen : AppCompatActivity() {
 
+    //Kuvan muuttuja
     var selectedImage : Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,9 +28,11 @@ class SecondScreen : AppCompatActivity() {
         setContentView(R.layout.activity_second_screen)
     }
 
+    //Annetaan kameralle ja gallerialle request numero, jolla yksilöidään tapahtuma
     val REQUEST_GALLERY = 1
     val REQUEST_CAMERA = 2
 
+    //Luodaan alert message
     fun selectImage(view: View) {
         val alert = AlertDialog.Builder(this)
 
@@ -45,11 +48,16 @@ class SecondScreen : AppCompatActivity() {
     }
 
     fun camera(view: View) {
+        //Tässä kysytään kameralle lupia
         if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(arrayOf(Manifest.permission.CAMERA), 2)
-        } else if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        }
+        //Tässä kysytään lupia kirjoittaa puhelimen tallennustilaan
+        else if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 3)
-        } else {
+        }
+        //Jos luvat on annettu pyydetään kameran aukaisua
+        else {
             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             try {
                 startActivityForResult(intent, REQUEST_CAMERA)
@@ -60,9 +68,12 @@ class SecondScreen : AppCompatActivity() {
     }
 
     fun gallery(view: View) {
+        //Kysytään gallerialle lupia
         if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 2)
-        } else {
+        }
+        //Jos luvat on annettu pyydetään gallerian aukaisua
+        else {
             val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             startActivityForResult(intent, REQUEST_GALLERY)
         }
@@ -70,18 +81,23 @@ class SecondScreen : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
 
+        //Jos requestcode on 1 avataan galleria, jos se on 2 niin kamera.
         if(requestCode == 1 && resultCode == Activity.RESULT_OK && data != null) {
             val image = data.data
 
             try {
+                //Galleriasta valittu kuva lisätään muuttujaan selectImage
                 selectedImage = MediaStore.Images.Media.getBitmap(this.contentResolver, image)
+                //Asetetaan kuva elementtiin
                 addImage.setImageBitmap(selectedImage)
             } catch (e: Exception) {
                 e.printStackTrace()
             }
         } else if(requestCode == 2 && resultCode == Activity.RESULT_OK && data != null) {
             try {
-                val selectedImage = data!!.extras?.get("data") as Bitmap
+                //Kameralla otettu kuva lisätään muuttujaan
+                selectedImage = data!!.extras?.get("data") as Bitmap
+                //Asetetaan kuva elementtiin
                 addImage.setImageBitmap(selectedImage)
             } catch (e: Exception) {
                 e.printStackTrace()
@@ -91,29 +107,38 @@ class SecondScreen : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
     }
 
+    //Tietojen tallennus
     fun saveNote(view: View) {
 
+        //Otetaan title ja content muuttujiin
         val noteTitle = titleField.text.toString()
         val noteContent = contentField.text.toString()
-
         val outputStream = ByteArrayOutputStream()
 
+        //Pakataan kuva
         selectedImage?.compress(Bitmap.CompressFormat.PNG, 50, outputStream)
+        //Lisätään kuva bytearray muuttujaan.
         val byteArray = outputStream.toByteArray()
 
         try {
+            //Luodaan database
             val database = this.openOrCreateDatabase("Notes", Context.MODE_PRIVATE, null)
+            //Luodaan taulut
             database.execSQL("create table if not exists note(title varchar, image blob, content varchar)")
-            val sqlString = "insert into note (title, image, content) values (?, ?)"
+            //SQL String tyhjillä muuttujilla
+            val sqlString = "insert into note (title, image, content) values (?, ?, ?)"
             val statement = database.compileStatement(sqlString)
+            //Lisätään sql lauseeseen muuttujat
             statement.bindString(1, noteTitle)
             statement.bindBlob(2, byteArray)
             statement.bindString(3, noteContent)
+            //tehdään työ
             statement.execute()
         } catch (e: Exception) {
             e.printStackTrace()
         }
 
+        //Kun nämä on tehty, niin palataan MainActivity näyttöön.
         val intent = Intent(applicationContext, MainActivity::class.java)
         startActivity(intent)
 
